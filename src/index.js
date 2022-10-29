b57ba14a4c69f1ab46f421abca90bb30f00cfd3d
KEHhyeon <kth050213@naver.com>
2022-10-29 23:20:08 +0900

    test2

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import InviteCode from "./pages/InviteCode";
+import MakeGroup from "./pages/MakeGroup";
+import Oauth from "./pages/ouath";
 import {
-  BrowserRouter,
-  Route,
-  Routes,
-} from "react-router-dom";
-import InviteCode from './pages/InviteCode';
-import MakeGroup from './pages/MakeGroup'
-import Oauth from './pages/ouath';import {
   RecoilRoot,
   atom,
   selector,
   userRecoilState,
   useRecoilValue,
-} from "recoil"
-import { Detail } from './pages/detail';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+} from "recoil";
+import { Detail } from "./pages/detail";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <RecoilRoot>
-  <BrowserRouter>
-    <Routes>
-      <Route path="/oauth" element={<Oauth/>}></Route>
-      <Route path="/" element={<App />}>
-        <Route path="/InviteCode" element={<InviteCode/>}/>
-        <Route path="/MakeGroup" element={<MakeGroup/>}/>
-        <Route path="/detail/:id" element={<Detail/>}/>
-      </Route>
-    </Routes>
-  </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/oauth" element={<Oauth />}></Route>
+        <Route path="/" element={<App />}>
+          <Route path="/InviteCode" element={<InviteCode />} />
+          <Route path="/MakeGroup" element={<MakeGroup />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </RecoilRoot>
 );
 
